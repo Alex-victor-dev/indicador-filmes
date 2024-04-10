6068ee1fde1bcfdab18118f618426428bf892205
Alex-victor-dev <alexvictoribeiro@gmail.com>
2024-04-10 15:16:44 -0300

    finaliza implementacao desafio

--- src/main/java/academy/wakanda/indicadorfilmes/domain/FilmeCategoria.java
+++ src/main/java/academy/wakanda/indicadorfilmes/domain/FilmeCategoria.java
@@ -13,14 +13,16 @@ import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
+@ToString
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class FilmeCategoria {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(columnDefinition = "uuid", name = "idFilme", updatable = false, unique = true, nullable = false)
 	private UUID id;
 	@Enumerated(EnumType.STRING)
