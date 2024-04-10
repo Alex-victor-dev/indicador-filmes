41451bec2097e4597fde2c4e50c700201b3907a5
Alex-victor-dev <alexvictoribeiro@gmail.com>
2024-04-10 12:47:10 -0300

    configura variavel de ambiente

--- src/main/java/academy/wakanda/indicadorfilmes/domain/FilmeCategoria.java
+++ src/main/java/academy/wakanda/indicadorfilmes/domain/FilmeCategoria.java
@@ -1,20 +1,30 @@
 package academy.wakanda.indicadorfilmes.domain;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FilmeCategoria {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(columnDefinition = "uuid" , name = "idFilme" , updatable = false , unique = true , nullable = false)
-    private UUID id;
-    @Enumerated(EnumType.STRING)
-    private FilmeCategoriaEnun categoria;
-    private Double temperaturaMinima;
-    private Double temperaturaMaxima;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "uuid", name = "idFilme", updatable = false, unique = true, nullable = false)
+	private UUID id;
+	@Enumerated(EnumType.STRING)
+	private FilmeCategoriaEnun categoria;
+	private Double temperaturaMinima;
+	private Double temperaturaMaxima;
 }
--- src/main/java/academy/wakanda/indicadorfilmes/infra/ClimaInfraClient.java
+++ src/main/java/academy/wakanda/indicadorfilmes/infra/ClimaInfraClient.java
@@ -5,6 +5,8 @@ import academy.wakanda.indicadorfilmes.application.api.LocalizacaoDTO;
 import academy.wakanda.indicadorfilmes.application.service.ClimaClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -13,14 +15,16 @@ import org.springframework.stereotype.Component;
 public class ClimaInfraClient implements ClimaClient {
 
     private final ClimaFeignClient climaFeignClient;
-    private String apiKey = "46c4bcf40974ce57aca2acf485c77ef3";
-    private String units = "metric";
+    @Value("${academy.wakanda.key}")
+    private String apiKey;
+    @Value("${academy.wakanda.units}")
+    private String units;
 
     @Override
     public ClimaDTO buscaClima(LocalizacaoDTO localizacao) {
         log.info( "[inicia] ClimaInfraClient - buscaClima" );
         log.info( "[localizacao] {}", localizacao );
-        ClimaDTO climaDTO = climaFeignClient.buscaClima( localizacao.getLongitude(), localizacao.getLatitude(), apiKey, units );
+        ClimaDTO climaDTO = climaFeignClient.buscaClima(localizacao.getLongitude(), localizacao.getLatitude(), apiKey, units );
         log.info( "[clima] {}", climaDTO);
         log.info( "[finaliza] ClimaInfraClient - buscaClima" );
         return climaDTO;
